56dcc316a0708e8bcd173b6bfb1aed371d91834d
Dmitriy Finozhenok <dmitry.finozhenok@aspose.com>
2020-01-10 14:17:49 +0900

    Improve container performance

--- runInDocker.windows.bat
+++ runInDocker.windows.bat
@@ -2,5 +2,5 @@ if exist out rmdir out /s /q
 mkdir out
 
 docker build -f Dockerfile.tests.windows -t aspose-words-cloud-cpp-tests:windows .
-docker run --rm -m 2g -v "%cd%/scripts:C:/scripts" -v "%cd%/out:C:/out" aspose-words-cloud-cpp-tests:windows cmd /c ".\scripts\runAll.bat %1 %2 %3
+docker run --rm -m 3g -v "%cd%/scripts:C:/scripts" -v "%cd%/out:C:/out" aspose-words-cloud-cpp-tests:windows cmd /c ".\scripts\runAll.bat %1 %2 %3
 exit /b %ERRORLEVEL%
