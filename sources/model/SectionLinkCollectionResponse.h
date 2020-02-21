47019869b66884cff2f4fc6a490476f58fe0aabc
wordscloud <aspose.cloud@aspose.com>
2020-02-20 23:23:58 -0800

    SDK regenerated by CI server [ci skip]

--- sources/model/SectionLinkCollectionResponse.h
+++ sources/model/SectionLinkCollectionResponse.h
@@ -39,7 +39,7 @@ namespace api {
 namespace models {
 
 /// <summary>
-/// This response should be returned by the service when handling: GET http://api.aspose.com/v4.0/words/Test.doc/sections.
+/// This response should be returned by the service when handling: GET https://api.aspose.cloud/v4.0/words/Test.doc/sections.
 /// </summary>
 class  SectionLinkCollectionResponse
     : public WordsResponse
