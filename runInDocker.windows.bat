37c8e5ed078b0b9f2e476197b8bd027b1a6fdfef
ivan.kishchenko <ivan.kishchenko@aspose.com>
2020-03-27 14:05:48 +0300

    Try to fix windows CI

--- runInDocker.windows.bat
+++ runInDocker.windows.bat
@@ -2,5 +2,5 @@ if exist out rmdir out /s /q
 mkdir out
 
 docker build -f Dockerfile.tests.windows -t aspose-words-cloud-cpp-tests:windows --isolation=hyperv .
-docker run --rm -m 3g -v "%cd%/scripts:C:/scripts" -v "%cd%/out:C:/out" --isolation=hyperv aspose-words-cloud-cpp-tests:windows cmd /c ".\scripts\runAll.bat %1 %2 %3"
+docker run --rm --env accept_eula=Y --memory 4G -v "%cd%/scripts:C:/scripts" -v "%cd%/out:C:/out" --isolation=hyperv aspose-words-cloud-cpp-tests:windows cmd /c ".\scripts\runAll.bat %1 %2 %3"
 exit /b %ERRORLEVEL%
