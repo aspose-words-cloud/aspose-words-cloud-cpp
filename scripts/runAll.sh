54dc9bb64fbddf19bf1ed2b8facc42f9d90bc306
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2020-05-21 17:25:33 +0700

    moved building dependencies to docker

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -1,18 +1,3 @@
-# Compile cppcheck
-cd /usr/bin/cppcheck-sources
-make SRCBUILD=build CFGDIR=/usr/bin/cppcheck-sources
-make install CFGDIR=/usr/bin/cppcheck-sources
-cp cppcheck /usr/bin/cppcheck
-
-# Compile cpprestsdk
-cd /
-git clone https://github.com/Microsoft/cpprestsdk.git
-mkdir build
-mkdir install
-cmake -DCPPREST_EXCLUDE_WEBSOCKETS=ON -DCPPREST_EXCLUDE_COMPRESSION=ON -DCPPREST_EXCLUDE_BROTLI=ON -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF -DCMAKE_INSTALL_PREFIX=install/cpprestsdk -S cpprestsdk -B build
-cmake --build build --config Debug --target install
-cmake --build build --config Release --target install
-
 # Compile aw
 mkdir -p aspose-words-cloud-cpp/build
 
