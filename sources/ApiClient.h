b7ae238e921b07403bae1193d2de4a8a22c713d7
Dmitriy Finozhenok <dmitry.finozhenok@aspose.com>
2020-06-05 18:22:21 +0900

    Update version

--- sources/ApiClient.h
+++ sources/ApiClient.h
@@ -130,7 +130,7 @@ private:
     utility::string_t m_AccessToken;
     std::array<std::pair<utility::string_t, utility::string_t>, 2> defaultHeaders =
     {
-        std::make_pair<utility::string_t, utility::string_t>(_XPLATSTR("x-aspose-client-version"), _XPLATSTR("20.5")),
+        std::make_pair<utility::string_t, utility::string_t>(_XPLATSTR("x-aspose-client-version"), _XPLATSTR("20.6")),
         std::make_pair<utility::string_t, utility::string_t>(_XPLATSTR("x-aspose-client"), _XPLATSTR("C++ SDK"))
     };
 
