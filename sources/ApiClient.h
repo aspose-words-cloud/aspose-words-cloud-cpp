7f8758c51954a1e836441b81197524b27a0f3d3f
evgeny.ivanov <evgeny.ivanov>
2020-08-04 23:01:19 -0500

    SDK regenerated by CI server [ci skip]

--- sources/ApiClient.h
+++ sources/ApiClient.h
@@ -129,7 +129,7 @@ private:
     utility::string_t m_AccessToken;
     std::array<std::pair<utility::string_t, utility::string_t>, 2> defaultHeaders =
     {
-        std::make_pair<utility::string_t, utility::string_t>(_XPLATSTR("x-aspose-client-version"), _XPLATSTR("20.7")),
+        std::make_pair<utility::string_t, utility::string_t>(_XPLATSTR("x-aspose-client-version"), _XPLATSTR("20.8")),
         std::make_pair<utility::string_t, utility::string_t>(_XPLATSTR("x-aspose-client"), _XPLATSTR("C++ SDK"))
     };
 
