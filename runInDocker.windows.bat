0dfe0073b18b536b3b92fe73fb1194c74400f323
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-03-11 14:57:42 +0300

    Remove unused files

--- runInDocker.windows.bat
+++
@@ -1,5 +0,0 @@
-if exist out rmdir out /s /q
-mkdir out
-
-docker run --rm --env accept_eula=Y --memory 4G -v "%cd%/scripts:C:/scripts" -v "%cd%/out:C:/out" aspose-words-cloud-cpp-tests:windows cmd /c ".\scripts\runAll.bat %1 %2 %3"
-exit /b %ERRORLEVEL%
