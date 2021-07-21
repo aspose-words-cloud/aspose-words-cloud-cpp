88698be4bc0b519abf312f8b49a997aeb6e2acbf
evgeny.ivanov <evgeny.ivanov>
2021-07-21 07:35:03 -0500

    SDK regenerated by CI server [ci skip]

--- tests/other/examples_test.h
+++ tests/other/examples_test.h
@@ -80,20 +80,3 @@ TEST_F(ExamplesTest, TestExampleAcceptAllRevisionsOnline) {
         fileWriter.close();
     }
 }
-
-TEST_F(ExamplesTest, TestExampleUpdateBookmark) {
-    auto wordsApi = getApi();
-    std::wstring remoteFileName = L"Sample.docx";
-    std::wstring bookmarkName = L"aspose";
-
-    auto testBookmarkData = std::make_shared< aspose::words::cloud::models::BookmarkData >();
-    testBookmarkData->setName(std::make_shared< std::wstring >(bookmarkName));
-    testBookmarkData->setText(std::make_shared< std::wstring >(L"New Bookmark Text"));
-
-    std::shared_ptr< requests::UpdateBookmarkRequest > updateBookmark(
-        new requests::UpdateBookmarkRequest(
-            std::make_shared< std::wstring >(remoteFileName), std::make_shared< std::wstring >(bookmarkName), testBookmarkData
-        )
-    );
-    wordsApi->updateBookmark(updateBookmark);
-}
