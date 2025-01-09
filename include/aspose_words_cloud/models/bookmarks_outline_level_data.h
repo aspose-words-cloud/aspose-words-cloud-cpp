0e449b6e34b2d68c6f684443df6961cad19b424e
jenkins <jenkins.aspose@gmail.com>
2025-01-09 01:27:31 -0600

    SDK regenerated by CI server [ci skip]

--- include/aspose_words_cloud/models/bookmarks_outline_level_data.h
+++ include/aspose_words_cloud/models/bookmarks_outline_level_data.h
@@ -1,6 +1,6 @@
 ﻿/** --------------------------------------------------------------------------------------------------------------------
 * <copyright company="Aspose" file="bookmarks_outline_level_data.h">
-*   Copyright (c) 2024 Aspose.Words for Cloud
+*   Copyright (c) 2025 Aspose.Words for Cloud
 * </copyright>
 * <summary>
 *   Permission is hereby granted, free of charge, to any person obtaining a copy
